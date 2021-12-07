111284732fba3a30af9f4b2cc233e84150d44835
ShanyHwang <shanyhwang@gmail.com>
2021-12-07 10:18:11 +0900

    practice Chart

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,54 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/viz/ui5/data/FlattenedDataset",
+    "sap/viz/ui5/controls/common/feeds/FeedItem"
+],  
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller, JSONModel, FlattenedDataset, FeedItem) {
+		"use strict";
+
+		return Controller.extend("iitp.zchart2401.controller.View1", {
+			onInit: function () {
+                let oModel = new JSONModel();
+                oModel.loadData("../model/data_chart.json");
+
+                let oVizFrame = this.getView().byId("idPiechart");
+                let oDataset = new FlattenedDataset({
+                    dimensions: [{
+                        name: "Skill Name",
+                        value: "{skillName}"
+                    }],
+                    measures: [{
+                        name: "Rate",
+                        value: "{rate}"
+                    }],
+                    data: {
+                        path: "/newSap"
+                    }
+                });
+                oVizFrame.setDataset(oDataset);
+                oVizFrame.setModel(oModel);
+
+                oVizFrame.setVizProperties({
+                    title: { text : "SAP Technology"},
+                    plotArea: {colorPalette: d3.scale.category20().range(),
+                    drawingEffect: "glossy"}
+                });
+                let feedSize = new FeedItem({
+                    uid: "size",
+                    type: "Measure",
+                    values: ["Rate"]
+                });
+                let feedColor = new FeedItem({
+                    uid: "color",
+                    type: "Dimension",
+                    values: ["Skill Name"]
+                });
+                oVizFrame.addFeed(feedSize);
+                oVizFrame.addFeed(feedColor);
+			}
+		});
+	});
